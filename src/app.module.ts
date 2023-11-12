47913c17e2c20ea947e9e741778081d4a7230b40
Marco Cortes <marcortes.stiven@gmail.com>
2023-11-12 01:02:34 -0600

    Products CRUD finished

--- src/app.module.ts
+++ src/app.module.ts
@@ -35,7 +35,7 @@ import { MenuOptionsModule } from './menu-options/menu-options.module';
       host: "localhost",
       port: 3306,
       username: "root",
-      password: "#Pln-7uj602",
+      password: "1234",
       database: "db_landingpage",
 
       autoLoadEntities: true,
